59be45aeb376186338ca2bdb82ff91eec596249b
Fariha Nuzhat Majumdar <nuzhatmila5@gmail.com>
2023-05-01 14:18:34 +0600

    setter injection

---
+++ 02-spring-boot-core/03-setter-injection/src/main/java/com/milacodesonmac/springcoredemo/rest/DemoController.java
@@ -0,0 +1,24 @@
+package com.milacodesonmac.springcoredemo.rest;
+
+import com.milacodesonmac.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for the dependency injection
+    @Autowired
+    public void setMyCoach(Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
