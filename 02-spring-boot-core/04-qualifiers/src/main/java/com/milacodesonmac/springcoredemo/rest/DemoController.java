6c08fc460887d8446956e97961650a9bb4b29a28
Fariha Nuzhat Majumdar <nuzhatmila5@gmail.com>
2023-05-01 14:32:34 +0600

    qualifier annotation in spring

---
+++ 02-spring-boot-core/04-qualifiers/src/main/java/com/milacodesonmac/springcoredemo/rest/DemoController.java
@@ -0,0 +1,25 @@
+package com.milacodesonmac.springcoredemo.rest;
+
+import com.milacodesonmac.springcoredemo.common.Coach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class DemoController {
+
+    // define a private field for the dependency
+    private Coach myCoach;
+
+    // define a constructor for the dependency injection
+    @Autowired
+    public DemoController(@Qualifier("trackCoach") Coach theCoach) {
+        myCoach = theCoach;
+    }
+
+    @GetMapping("/dailyworkout")
+    public String getDailyWorkout(){
+        return myCoach.getDailyWorkout();
+    }
+}
